e7742c17adecd4867231a9fea646f5915ecee733
Isaque Fernando <isaquefernando10@gmail.com>
2018-10-16 20:24:45 -0300

    Procedimentos Funcional

--- Proc/Procedimento.java
+++ Proc/Procedimento.java
@@ -9,51 +9,58 @@ public class Procedimento {
    String nome;     										// nome do procedimento  
    Vector comandos;    									// vetor de comandos 
    String [] parametros;									// parametros formais 
-   String [] variaveisLocal;								// variáveis local
+   String [] variaveisLocal;								// variaveis local
    static Memoria global = new Memoria();			// variaveis globais
 		 
 		 
-   public Procedimento( String n, String [] p ){
-      nome= n;
-      parametros= p; 
-   }
+    public Procedimento( String n, String [] p ){
+        nome= n;
+        parametros= p; 
+    }
 		
-   public String getNome(){
-      return nome;
-   }
+    public String getNome(){
+        return nome;
+    }
 		 
-   // Atribuir valor as variáveis globais.
-   public static void setVariaveisGlobal( String [] vG ){	
-      char var;
-   		
-   }
+   // Atribuir valor as variaveis globais.
+    public static void setVariaveisGlobal( String [] vG ){	
+        char var;
+   		for(int i=0; vG[i]!=null; i++){
+            var = vG[i].charAt(0);
+            global.var[var - 97] = 0;
+        }
+    }
 		
-   public void setVariaveisLocal( String [] vL ){
-      this.variaveisLocal = vL;	
-   }
+    public void setVariaveisLocal( String [] vL ){
+        this.variaveisLocal = vL;	
+    }
 		
-   public void setListaComandos( Vector c ){
-      this.comandos = c;
-   }
+    public void setListaComandos( Vector c ){
+        this.comandos = c;
+    }
 		
-   public void executa(double [] argumentos) {
-      Memoria local = new Memoria(); 				// variaveis local
-      char var;
+    public void executa(double [] argumentos) {
+        Memoria local = new Memoria(); 				// variaveis local
+        char var;
    	  
-   	  // adiciona variáveis locais na memória local
-      if( variaveisLocal != null ){	
-      										
-      }
+   	    // adiciona variaveis locais na memoria local
+        if( variaveisLocal != null ){	
+      	     char variavel;
+             for(int i=0; variaveisLocal[i]!=null; i++){
+                variavel = variaveisLocal[i].charAt(0); // Recebendo a variavel.
+                local.var[variavel - 97] = 0; // Atribuindo variavel.
+             }	
+        }
         
-        // associa argumentos a seus respectivos parâmetros formais
-        
-        
-        
-        										
+        // associa argumentos a seus respectivos parametros formais
+        for(int i=0; parametros[i]!=null; i++){
+            var = parametros[i].charAt(0); // Atribuimos a var a variavel local criada.
+            local.var[var - 97] = argumentos[i]; // Atribuimos a variavel o ser valor passado como argumento.
+        }										
             		
-      int pc= 0;
-      do {
-         pc=  ((Comando) comandos.elementAt(pc)).executa(local, global);
-      } while (pc < comandos.size() );
+        int pc= 0;
+        do {
+            pc=  ((Comando) comandos.elementAt(pc)).executa(local, global);
+        } while (pc < comandos.size() );
    }
 }
--- tiny.java
+++ tiny.java
@@ -7,7 +7,7 @@
    
        public static void main(String[] args) {
       
-         Interpretador inter= new Interpretador("/home/daniel/Projetos/LP/TP1_Lp/Testes/proc1.tiny");
+         Interpretador inter= new Interpretador("/home/isaque/Projects/TP1_Lp/Testes/proc6.tiny");
          //if (args.length == 1) {
             inter.leArquivo();
             inter.executa();
